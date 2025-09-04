e596396f694fe0f5587e56b1631241a7284fa3d5
daniel <dantiptoe@gmail.com>
2025-09-03 20:05:05 -0600

    added ManualEdit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function App() {
   return <TurnTracker />;
@@ -21,9 +21,14 @@ const savedRotation = (localStorage.getItem("rotation") as string)?.split("\n")
 
 function TurnTracker() {
   const [rotation, setRotation] = useState(savedRotation);
+  /** rotation in localStorage ready form */
+  function convertRotation() {
+    return rotation.map(([item]) => item).join("\n");
+  }
+
   useEffect(() => {
     if (rotation.length === 0) localStorage.removeItem("rotation");
-    else localStorage.rotation = rotation.map(([item]) => item).join("\n");
+    else localStorage.rotation = convertRotation();
   }, [rotation]);
 
   function rotate() {
@@ -57,8 +62,43 @@ function TurnTracker() {
       </label>&nbsp;
       <button onClick={addNewItem}>Add</button>
     </div>
+    <ManualEdit rotation={convertRotation()} setRotation={setRotation} />
   </>;
 }
 function ListItem({ val }: { val: string }) {
   return <li>{val}</li>;
+}
+
+
+function ManualEdit(
+  { rotation, setRotation }:
+  { rotation: string, setRotation: (rotation: Item[]) => void }
+) {
+  const [text, setText] = useState("");
+  const dialog = useRef(null as HTMLDialogElement | null);
+
+
+  function showModal() {
+    dialog.current?.showModal();
+    setText(rotation); 
+  }
+
+
+  return <div>
+    <button onClick={showModal}>Manual Edit</button>
+    <dialog ref={dialog} onClose={() => {
+      setRotation(text.split("\n").map(
+        item => newItem(item)) ?? []
+      )
+    }}>
+      <div>
+        <textarea
+          rows={10}
+          value={text}
+          onChange={e => { setText(e.target.value); }}
+        ></textarea>
+      </div>
+      <button onClick={() => { dialog.current?.close(); }}>Close</button>
+    </dialog>
+  </div>;
 }
